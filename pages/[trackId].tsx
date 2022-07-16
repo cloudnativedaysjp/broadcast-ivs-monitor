4c16d16b9e797255fe501d7f0effa71d6b8c0e04
Kazuto Kusama <jacopen@gmail.com>
2022-07-17 01:01:03 +0900

    Implement player

--- pages/[trackId].tsx
+++ pages/[trackId].tsx
@@ -1,11 +1,13 @@
 import type { NextPage } from 'next'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { useRouter } from 'next/router'
-
+import { Player } from '../src/components/player'
 
 const Track: NextPage = () => {
   const router = useRouter()
   const [trackId, setTrackId] = useState<string>()
+  const videoElement = useRef<HTMLVideoElement>(null)
+
   useEffect(() => {
     if (router.asPath !== router.route) {
       const { trackId } = router.query
@@ -15,7 +17,7 @@ const Track: NextPage = () => {
 
   return (
     <div>
-        { trackId }
+        <Player playBackUrl={'https://d3pun3ptcv21q4.cloudfront.net/medialive/o11y2022/talks/1349/1349.m3u8'} />
     </div>
   )
 }
