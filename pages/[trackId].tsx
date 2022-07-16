7437e0a08f4608f35543672cf9ba4239a84de646
Kazuto Kusama <jacopen@gmail.com>
2022-07-17 01:33:47 +0900

    Overlay trackname

--- pages/[trackId].tsx
+++ pages/[trackId].tsx
@@ -2,6 +2,18 @@ import type { NextPage } from 'next'
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import { Player } from '../src/components/player'
+import { css } from "@mui/styled-engine";
+
+const Overlay = css({
+  position: "absolute",
+  bottom: 10,
+  right: 10,
+  color: 'rgba(255,255,255,0.5)',
+  zIndex: 2,
+  textShadow: '0 0 10px rgba(0,0,0,0.5)',
+  fontSize: '8em',
+
+});
 
 const Track: NextPage = () => {
   const router = useRouter()
@@ -16,7 +28,7 @@ const Track: NextPage = () => {
 
   return (
     <div>
-      <h2>You are playing {trackId}</h2>
+      <div css={Overlay}>Track {trackId}</div>
       <Player
         playBackUrl={
           'https://d3pun3ptcv21q4.cloudfront.net/medialive/o11y2022/talks/1349/1349.m3u8'
